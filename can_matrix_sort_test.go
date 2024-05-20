fac206f18977675c701c99f40f2f4d13a1ed3116
vilinvil <ivn15072002@gmail.com>
2024-05-20 19:20:01 +0300

    add test big matrix

--- can_matrix_sort_test.go
+++ can_matrix_sort_test.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 )
+
+const dimensionBigMatrix = 100
+
+var firstLineBigMatrix = fmt.Sprintf("%d\n", dimensionBigMatrix) //nolint:gochecknoglobals
+
+func generateBigMatrix() string {
+	line := "1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1" +
+		" 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1\n"
+
+	result := make([]byte, len(firstLineBigMatrix)+len(line)*100)
+
+	copy(result, firstLineBigMatrix)
+
+	for i := 0; i < dimensionBigMatrix; i++ {
+		copy(result[i*len(line)+len(firstLineBigMatrix):], line)
+	}
+
+	return string(result)
+}
 
 type testCaseCanMatrixSort struct {
 	name           string
@@ -15,53 +35,58 @@ var testCases = [...]testCaseCanMatrixSort{ //nolint:gochecknoglobals
 	{
 		name: "Example 1 from task.md",
 		input: `2
-1 2
-2 1`,
+	1 2
+	2 1`,
 		expectedOutput: true,
 	},
 	{
 		name: "Example 2 from task.md",
 		input: `3
-10 20 30
-1 1 1
-0 0 1`,
+	10 20 30
+	1 1 1
+	0 0 1`,
 		expectedOutput: false,
 	},
 	{
 		name: "big values",
 		input: `2
-1000000000 1000000002
-1000000002 1000000000`,
+	1000000000 1000000002
+	1000000002 1000000000`,
 		expectedOutput: true,
 	},
 	{
 		name: "zero values",
 		input: `3
-0 0 0
-0 0 0
-0 0 0`,
+	0 0 0
+	0 0 0
+	0 0 0`,
 		expectedOutput: true,
 	},
 	{
 		name: "empty containers",
 		input: `3
-0 0 1
-0 0 0
-0 0 0`,
+	0 0 1
+	0 0 0
+	0 0 0`,
 		expectedOutput: true,
 	},
 	{
 		name: "can not sort containers",
 		input: `3
-0 0 3
-0 3 0
-1 0 1`,
+	0 0 3
+	0 3 0
+	1 0 1`,
 		expectedOutput: false,
 	},
 	{
 		name: "dimension matrix = 1",
 		input: `1
-4`,
+	4`,
+		expectedOutput: true,
+	},
+	{
+		name:           "big matix",
+		input:          generateBigMatrix(),
 		expectedOutput: true,
 	},
 }
